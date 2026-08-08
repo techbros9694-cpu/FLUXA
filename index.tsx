2e116d2c36332141c9803237abb5acf9aac4cba3
Tech Bro's <techbros9694@gmail.com>
2026-08-09 01:50:31 +0530

    edited some errors

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import App from "./src/App";
+import "./src/index.css";
 
 const rootElement = document.getElementById("root");
 if (rootElement && !rootElement.innerHTML) {
