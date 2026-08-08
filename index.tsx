2cd767261bd58f77e6940a3cff1d722f4187bcfb
Tech Bro's <techbros9694@gmail.com>
2026-08-09 01:57:14 +0530

    JavaScript error

--- index.tsx
+++ index.tsx
@@ -1,14 +1,2 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./src/App";
-import "./src/index.css";
+import "./src/main";
 
-const rootElement = document.getElementById("root");
-if (rootElement && !rootElement.innerHTML) {
-  const root = createRoot(rootElement);
-  root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-}
